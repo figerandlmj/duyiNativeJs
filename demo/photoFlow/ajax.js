15771f7cf8f6216e13a2128bfbc734c23af9b5ba
figerandlmj <2270200373@qq.com>
2018-03-30 18:30:19 +0800

    update

---
+++ demo/photoFlow/ajax.js
@@ -0,0 +1,35 @@
+function ajax(method, url, data, callback, flag){
+	var xhr = null;
+	if(window.XMLHttpRequest){
+		xhr = new XMLHttpRequest();
+	}else{
+		xhr = new ActiveXObject('Microsoft.XMLHttp');
+	}
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState == 4){
+			if(xhr.status == 200) {
+				callback(xhr.responseText);
+			}else{
+				console.log('error:' + xhr.status);
+			}
+		}
+	}
+	method = method.toUpperCase();
+	data = data && getParams(data);
+	if(method == 'GET') {
+		var timer = new Date().getTime();
+		xhr.open(method, url + '?' + data + '&timer=' + timer, flag);
+		xhr.send();
+	}else if(method == 'POST') {
+		xhr.open(method, url, flag);
+		xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+		xhr.send(data);
+	}
+}
+function getParams(data) {
+    var arr = [];
+    for (var param in data){
+        arr.push(encodeURIComponent(param) + '=' +encodeURIComponent(data[param]));
+    }
+    return arr.join('&');
+}
---
+++ demo/photoFlow/index.js
@@ -0,0 +1,66 @@
+
+var oLi = document.getElementsByTagName('li');
+
+var data = {
+	cpage: 1
+};
+
+var lock = true;
+
+ajaxFun();
+
+function ajaxFun() {
+	if(lock) {
+		lock = false;
+		ajax('GET', './data.php', data, getData, true);
+		data.cpage ++;
+	}
+}
+
+
+window.onscroll = function() {
+	var scrollHeight = document.documentElement.scrollTop || document.body.scrollTop,
+		clientHeight = document.documentElement.clientHeight || document.body.clientHeight,
+		minHeight = oLi[minListIndex(oLi)].offsetHeight;
+	if(scrollHeight + clientHeight >= minHeight){
+		ajaxFun();
+	}
+}
+
+// 渲染数据
+function getData(data) {
+	console.log(data);
+	var value = JSON.parse(data);
+	value.forEach(function(ele, index) {
+		var index = minListIndex(oLi);
+		var oDiv = document.createElement('div'),
+			oP = document.createElement('p'),
+			oImg = new Image();
+		oImg.src = ele.preview;
+		oP.innerHTML = ele.title;
+		oDiv.className = 'item';
+		oImg.style.height = ele.height / ele.width * 200 + 'px';
+		oDiv.appendChild(oImg);
+		oDiv.appendChild(oP);
+		oLi[index].appendChild(oDiv);
+		oImg.onerror = function() {
+			oImg.style.margin = '-1px';
+			oImg.style.width = '202px';
+		}
+	});
+	lock = true;
+}
+
+// 获取li最短的那一列的index
+function minListIndex(dom){
+	var minListHeight = dom[0].offsetHeight,
+		index = 0,
+		len = dom.length;
+	for(var i = 1; i < len; i ++) {
+		if(dom[i].offsetHeight < minListHeight){
+			minListHeight = dom[i];
+			index = i;
+		}
+	}
+	return index;
+}
